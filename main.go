ee3b47bf2a5dafd56ad9ff84ee7bf5fd0b28dfc4
CaulpnryDC <kris.bunney@gmail.com>
2022-02-10 02:39:20 +0000

    Fixed http client download, started logging addition

--- main.go
+++ main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
+var url = "https://filesamples.com/samples/code/json/sample2.json"
+
 func main() {
-	fmt.Println("Established")
+	logFileName := "log/demo.json"
+	f, err := os.OpenFile(logFileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		log.Output(zerolog.ConsoleWriter{Out: f})
+	}
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	log.Info().Msg("Established")
+	log.Info().Msg("Creating output file")
+	out, err := os.Create("output.json")
+	if err != nil {
+		log.Error().Err(err)
+	}
+	defer out.Close()
+
+	log.Info().Msg("Downloading File")
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+
+	log.Info().Msg("Saving output to file")
+	_, err = io.Copy(out, resp.Body)
 }
